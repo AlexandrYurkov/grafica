493538682355bf6222e7ca9dd16cd9530176d5c3
Alexandr Yurkov <alexandryurkov1@gmail.ru>
2021-09-02 18:05:07 +0300

    3d

--- grafica/fdf.h
+++ grafica/fdf.h
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <mlx.h>
+#include <math.h>
 #include <stdio.h> // удалить, заменить на ft_printf
 #include "libft2/libft.h"
 
@@ -13,12 +14,17 @@ typedef struct
     int h;
     int w;
     int **d;
-    // float x;
-    // float y;
-    // float z;
+    float x;
+    float y;
+    float z;
+    int zoom;
+    int color;
+
  
 }               fdf;
 
 int *map_read(char *file, fdf *matrix_map);
+void print_map (fdf *matrix_map);
+void point_on_the_map(float x, float x1, float y, float y1, fdf *matrix_map);
 
 #endif
--- grafica/print_map.c
+++ grafica/print_map.c
@@ -15,9 +15,26 @@ int max (int a, int b)
         return (b);
 }
 
-void get_zoom(float *x, float *x1, float *y, float *y1)// можно добавить переменную zoom в структуру и изменять ее
+void get_zoom(float *x, float *x1, float *y, float *y1, fdf *matrix_map)// можно добавить переменную zoom в структуру и изменять ее
 {
+    *x *= matrix_map->zoom;
+    *x1 *= matrix_map->zoom;
+    *y *= matrix_map->zoom;
+    *y1 *= matrix_map->zoom;
+}
 
+int get_color(int z, int z1, fdf *matrix_map)
+{
+    if(z > 0 || z1 > 0)
+        return (0xFF0000);
+    else
+        return (0xffffff);
+}
+
+void get_3d(float *x, float *y, int z)
+{
+    *x = (*x -*y) * cos(0.8);
+    *y = (*x + *y) * sin (0.8) - z;
 }
 
 void point_on_the_map(float x, float x1, float y, float y1, fdf *matrix_map)
@@ -25,22 +42,27 @@ void point_on_the_map(float x, float x1, float y, float y1, fdf *matrix_map)
     float x_step;
     float y_step;
     int maxinus;
+    int z;
+    int z1;
 
-    get_zoom(&x, &x1, &y, &y1);
-
+    z = matrix_map->d[(int)x][(int)y];
+    z1 = matrix_map->d[(int)x1][(int)y1];
+    matrix_map->color = get_color(z, z1, matrix_map);
+    get_zoom(&x, &x1, &y, &y1, matrix_map);
     x_step = x1 - x;
     y_step = y1 - y;
     maxinus = max(mod(x_step), mod(y_step));
     x_step /= maxinus;
     y_step /= maxinus;
-
-    while ((int)(x -x1)) || (int)(y - y1))
+    get_3d(&x, &y, z);
+    get_3d(&x1, &y1, z1);
+    // вставить изменения по event x, y, x1, y1
+    while ((int)(x -x1) || (int)(y - y1))
     {
-        mlx_pixel_put(matrix_map->mlx_ptr, matrix_map->win_ptr, x, y, 0xfffffff);
+        mlx_pixel_put(matrix_map->mlx_ptr, matrix_map->win_ptr, x, y, matrix_map->color);
         x += x_step;
         y += y_step;
     }
-    
 }
 
 void print_map (fdf *matrix_map)
